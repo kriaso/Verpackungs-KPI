a9e969036359e760d4ac5ec5330e8adb8012a80e
kriaso <philip_weinzierl@live.de>
2025-04-03 09:11:29 +0200

    Update logistik_kpi_dashboard.py

--- logistik_kpi_dashboard.py
+++ logistik_kpi_dashboard.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 
 st.set_page_config(page_title='Logistik KPI-Dashboard', layout='wide')
 st.title('Logistik KPI-Dashboard')
